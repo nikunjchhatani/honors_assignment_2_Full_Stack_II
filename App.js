4ec426a1496aace91f20adddcb3c5706400d1c19
nikunjchhatani <149239126+nikunjchhatani@users.noreply.github.com>
2024-12-05 11:36:41 +0530

    all files uploaded

--- App.js
+++ App.js
@@ -6,12 +6,12 @@ import { FontAwesome5, MaterialIcons, Feather } from '@expo/vector-icons';
 
 // Sample product data for FlatList
 const products = [
-  { id: '1', name: 'AERO SPORT INFINITY PRO', price: 'Rp400.000', image: 'https://rukminim2.flixcart.com/image/850/1000/xif0q/shoe/6/a/v/9-5-107805-puma-rickie-orange-silver-white-original-imagpehhqrsct3t5.jpeg?q=90&crop=false' },
-  { id: '2', name: 'SPORT - INVINCIBLE PRO', price: 'Rp399.000', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThfrz2Rym2L38tqhmcegbhtsAk2GrAVfXTnQ&s' },
-  { id: '3', name: 'SPORT SNEAKERS - White', price: 'Rp200.000', image: 'https://assets.myntassets.com/w_412,q_60,dpr_2,fl_progressive/assets/images/2024/AUGUST/28/OZAFTDQU_be905f2aef1444cbb280d0cfeaab649b.jpg' },
-  { id: '4', name: 'SPORT - INVINCIBLE MAX', price: 'Rp393.000', image: 'https://assets.myntassets.com/w_412,q_60,dpr_2,fl_progressive/assets/images/28423358/2024/3/21/c99e213d-2b23-4cf6-823d-d88dfaeed3531711043994992NikeInvincible3MensRoadRunningShoes1.jpg' },
-  { id: '5', name: 'AIR SPORT', price: 'Rp200.000', image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXxlbnwwfHwwfHx8MA%3D%3D' },
-  { id: '6', name: 'SPORT MAX', price: 'Rp350.000', image: 'https://images.unsplash.com/photo-1512374382149-233c42b6a83b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHNob2V8ZW58MHx8MHx8fDA%3D' }
+  { id: '1', name: 'AERO SPORT INFINITY PRO', price: 'Rs 7,999', image: 'https://rukminim2.flixcart.com/image/850/1000/xif0q/shoe/6/a/v/9-5-107805-puma-rickie-orange-silver-white-original-imagpehhqrsct3t5.jpeg?q=90&crop=false' },
+  { id: '2', name: 'SPORT - INVINCIBLE PRO', price: 'Rs 5,999', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThfrz2Rym2L38tqhmcegbhtsAk2GrAVfXTnQ&s' },
+  { id: '3', name: 'SPORT SNEAKERS - White', price: 'Rs 20,00', image: 'https://assets.myntassets.com/w_412,q_60,dpr_2,fl_progressive/assets/images/2024/AUGUST/28/OZAFTDQU_be905f2aef1444cbb280d0cfeaab649b.jpg' },
+  { id: '4', name: 'SPORT - INVINCIBLE MAX', price: 'Rs 8,999', image: 'https://assets.myntassets.com/w_412,q_60,dpr_2,fl_progressive/assets/images/28423358/2024/3/21/c99e213d-2b23-4cf6-823d-d88dfaeed3531711043994992NikeInvincible3MensRoadRunningShoes1.jpg' },
+  { id: '5', name: 'AIR SPORT', price: 'Rs 5,499', image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXxlbnwwfHwwfHx8MA%3D%3D' },
+  { id: '6', name: 'SPORT MAX', price: 'Rs 6,999', image: 'https://images.unsplash.com/photo-1512374382149-233c42b6a83b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHNob2V8ZW58MHx8MHx8fDA%3D' }
 ];
 
 // Home Screen Component
